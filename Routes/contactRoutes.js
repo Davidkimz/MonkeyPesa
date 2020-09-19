6516dbe0c8241f34b9948d5c8b4d12f93a7ed8e4
emma <enabojohn@gmail.com>
2020-09-19 18:54:26 +0300

    Contact routes to hit end points only if user is Authorised

---
+++ Routes/contactRoutes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const Contact= require("../models/contactModel")
+const bcrypt  = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const auth = require("../middleware/auth")
+
+const ContactController = require("../controllers/ContactControllers");
+const contactController = new ContactController();
+
+//CREATE NEW CONTACT
+router.post("/", auth,(req, res) => contactController.createContact(req, res));
+router.get("/",  auth, (req, res) => contactController.getAllContacts(req, res));
+router.get("/:id",  auth, (req, res) => contactController.getContact(req, res));
+router.patch("/:id",  auth, (req, res) => contactController.updateContact(req, res));
+router.delete("/:id", auth, (req, res) => contactController.deleteContact(req, res));
+
+
+
+module.exports = router;
